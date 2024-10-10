96910938659e91a8b0c5e250cdb51d7cb00c3919
nmbabor <nmbabor60@gmail.com>
2024-10-11 00:52:14 +0600

    Make Scheduler for notification send

---
+++ app/Console/Commands/SendVaccinationEmails.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Mail\VaccinationScheduledMail;
+use App\Models\VaccinationRegistration;
+use Carbon\Carbon;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Mail;
+
+class SendVaccinationEmails extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'vaccination:send-emails';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Send vaccination appointment emails to users';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+         // Get the current date and time
+         $currentDateTime = Carbon::now();
+        
+         // Get the date for tomorrow at 9 PM
+         $tomorrowDate = $currentDateTime->addDay()->setTime(21, 0, 0);
+ 
+         // Get all registrations that are scheduled for tomorrow
+         $registrations = VaccinationRegistration::whereDate('scheduled_date', $tomorrowDate->toDateString())->get();
+ 
+         foreach ($registrations as $registration) {
+             Mail::to($registration->email)->send(new VaccinationScheduledMail($registration));
+         }
+ 
+         $this->info('Vaccination emails sent successfully!');
+    }
+}
